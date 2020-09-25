061bc49fbb6e48146c1c8aabef87dc6c262f48b4
Tarun Bisht <tarunbisht252000@gmail.com>
2020-09-26 04:19:24 +0530

    purify markdown

--- routes/articles.js
+++ routes/articles.js
@@ -6,25 +6,31 @@ router.get("/new", (req, res) => {
   res.render("articles/new", { article: new Article() });
 });
 
-router.get("/:id", (req, res) => {
-  const article = Article.findById(req.params.id);
+router.get("/:slug", async (req, res) => {
+  const article = await Article.findOne({ slug: req.params.slug });
   if (article == null) {
+    res.redirect("/");
   }
   res.render("articles/show", { article: article });
 });
 
 router.post("/", async (req, res) => {
-  const article = new Article({
+  let article = new Article({
     title: req.body.title,
     description: req.body.description,
     markdown: req.body.markdown,
   });
   try {
     article = await article.save();
-    res.redirect(`/articles/${article.id}`);
+    res.redirect(`/articles/${article.slug}`);
   } catch (error) {
     console.log(error);
     res.render("articles/new", { article: article });
   }
+});
+
+router.delete("/:id", async (req, res) => {
+  await Article.findByIdAndDelete(req.params.id);
+  res.redirect("/");
 });
 module.exports = router;
--- server.js
+++ server.js
@@ -1,30 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Article = require("./models/article");
+const methodOverride = require("method-override");
 const app = express();
 const articleRouter = require("./routes/articles");
 
 mongoose.connect("mongodb://localhost/blog", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
 
-app.get("/", (req, res) => {
-  const articles = [
-    {
-      title: "Test Article 1",
-      createdAt: new Date(),
-      description: "Test Description 1",
-    },
-    {
-      title: "Test Article 2",
-      createdAt: new Date(),
-      description: "Test Description 1",
-    },
-  ];
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({
+    createdAt: "desc",
+  });
   res.render("articles/index", { articles: articles });
 });
 
